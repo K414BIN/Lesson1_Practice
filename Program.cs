82bad400e389790b1e54d3df3ad937c9b56a7d3d
Alexander Kalabin <k414bin@yandex.ru>
2021-05-03 13:56:00 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Lesson1_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
