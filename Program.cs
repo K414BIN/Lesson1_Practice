c1e42263c13029988bfcbab94559b96d04b5300d
Alexander Kalabin <k414bin@yandex.ru>
2021-05-11 13:14:35 +0300

    final version

--- Program.cs
+++ Program.cs
@@ -15,55 +15,44 @@ namespace Lesson1_Practice
         public static string mainAddress = "https://jsonplaceholder.typicode.com/posts";
         public static string fname ="result.txt";
 
+        private static int magicNumber1=4;
+        private static int magicNumber2=13;
+
         static async Task Main(string[] args)
         {
               try
               {
-                var value1 =   GetOnePost(4);
-                var value2 =   GetOnePost(5);
-                var value3 =   GetOnePost(6);
-                var value4 =   GetOnePost(7);
-                var value5 =   GetOnePost(8);
-                var value6 =   GetOnePost(9);
-                var value7 =   GetOnePost(10);
-                var value8 =   GetOnePost(11);
-                var value9 =   GetOnePost(12);
-                var value10 =  GetOnePost(13);
                 var tasks = new List<Task<Post>>() ;
-                tasks.AddRange(new [] {value1,value2,value3,value4,value5,value6,value7,value8,value9,value10});
+                for (int i=magicNumber1 ;i<=magicNumber2;i++)  tasks.AddRange(new [] {GetPost(i) });                
                 await Task.WhenAll(tasks);
-                // Круто - try внутри try !!!
                 try
                     {
-                    // Для того, чтобы просто вывести на экран - закомментируйте строки перед и после "плюсов"
                     // ************************
                             FileStream fs = new FileStream(fname, FileMode.Create);
                             TextWriter tmp = Console.Out;
-                            StreamWriter sw = new StreamWriter(fs);
-                            Console.SetOut(sw);
+                            StreamWriter ecrivain_streaming  = new StreamWriter(fs);
+                            Console.SetOut( ecrivain_streaming );
                     //+++++++++++++++++++++++++++++++
-                            foreach ( var z in tasks)
+                            foreach ( var variable  in tasks)
                             {
-                              Console.WriteLine(z.Result.UserId);
-                              Console.WriteLine(z.Result.Id);
-                              Console.WriteLine(z.Result.Title);
-                              Console.WriteLine(z.Result.Body);
+                              Console.WriteLine(variable.Result.UserId);
+                              Console.WriteLine(variable.Result.Id);
+                              Console.WriteLine(variable.Result.Title);
+                              Console.WriteLine(variable.Result.Body);
                               Console.WriteLine();
                             }
                     //+++++++++++++++++++++++++++++
                             Console.SetOut(tmp);
-                            sw.Close();
+                             ecrivain_streaming.Close();
                     // ************************
                      }
-                // вспомогательный try
                 catch(IOException e)
                 {
                     TextWriter errorWriter = Console.Error;
                     errorWriter.WriteLine(e.Message);
                 }
               }
-            // главный try
-               catch (Exception ex)
+              catch (Exception ex)
               {
                     Console.WriteLine(ex.Message);
               }
@@ -71,9 +60,9 @@ namespace Lesson1_Practice
                 Console.WriteLine($"All result stored in {fname} file!");
         }
 
-       static async Task <Post> GetOnePost(int num)
+       static async Task <Post> GetPost(int id)
        {
-            var response = await client.GetAsync(mainAddress+"/"+ num.ToString());
+            var response = await client.GetAsync(mainAddress+"/"+ id.ToString());
             if (!response.IsSuccessStatusCode)
             {
                 Console.WriteLine("Error while getting an http reponse!");
